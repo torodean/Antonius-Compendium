45f2132fefc9082056f1c92f10f0bb48c240df66
Antonius Torode <24546058+torodean@users.noreply.github.com>
2024-10-29 14:27:15 -0500

    Update commitAll.sh

--- commitAll.sh
+++ commitAll.sh
@@ -1,24 +1,34 @@
 #!/bin/sh
-if [ $# -eq 0 ]
+echo "...Loading."
+echo "...Created by Antonius Torode."
+echo "...Please make sure you are connected to the Internet."
+echo "...If this script isn't working, make sure EOL Conversion is set to Unix."
+echo "..."
+git pull
+echo "..."
+git status
+echo "..."
+git add -A
+echo "..."
+git status
+echo "..."
+commit_message=""
+while getopts ":m:" opt; do
+  case ${opt} in
+    m ) commit_message="$OPTARG";;
+    \? ) echo "Invalid option: -$OPTARG" 1>&2; exit 1;;
+    : ) echo "Option -$OPTARG requires an argument." 1>&2; exit 1;;
+  esac
+done
+if [ -z "$commit_message" ]
 then
-   echo "No arguments supplied"
+  git commit -m "$1"
 else
-   echo "...Loading."
-   echo "...Created by Antonius Torode."
-   echo "...Please make sure you are connected to the Internet."
-   echo "...If this script isn't working, make sure EOL Conversion is set to Unix."
-   echo "..."
-   git status
-   echo "..."
-   git add -A
-   echo "..."
-   git status
-   echo "..."
-   git commit -m "$1"
-   echo "..."
-   git status
-   echo "..."
-   git push
-   echo "..."
-   echo "...Finished."
+  git commit -m "$commit_message"
 fi
+echo "..."
+git status
+echo "..."
+git push
+echo "..."
+echo "...Finished."
